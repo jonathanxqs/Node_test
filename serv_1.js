b9868a6acf78789c81eedc650f652e68a56de22a
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-16 14:19:18 -0400

    Serv1

--- serv_1.js
+++ serv_1.js
@@ -9,6 +9,8 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log('Be aware of using CPU and port!');
 });
